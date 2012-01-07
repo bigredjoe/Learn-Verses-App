ea39d6c4e50de7d88dfe859652359cee4edc8f2f
Joseph Doty <bigred@josephdoty.com>
2012-01-07 13:53:01 -0600

    Polishing for Github publish

---
+++ README.md
@@ -0,0 +1,19 @@
+# Learn Verses App #
+
+A flashcard-like app with audio playback to aid in the memorization of Bible verses.
+
+I wrote this app as a personal project in my first baby step into mobile/app development. Hope you enjoy it!
+
+## Features ##
+* Works both in the Safari browser and in the app.
+* It has persistent local storage of the verses even if app is force-quit.
+* Verse by verse audio to aid the auditory memory.
+* Uses Phonegap framework
+
+### Challenges of Development ###
+* Having to interact with an API not formulated for javascript.
+* Creating a partial function for reading local files for Handlebars.js and initializing callback javascript.
+* Refactoring and optimizing my code again and again.
+
+### Take Aways ###
+ I learned the basics of LESS CSS, Handlebars.js, Jquery Mobile, XML, and Local Storage while working on the project. And in the end I had a working app!
--- www/js/functions.js
+++ www/js/functions.js
@@ -1,10 +1,10 @@
 var fetchverses = function(callBack) {
 		//Split Up the Passage Input for the ESV API
 		var passage = $("#passage").val().split(" ");
-		localStorage.previousPassage = $("#passage").val();
+		localStorage.Passage = $("#passage").val();
 		//Setting Title H1
 		$("#title").text("Loading Scriptures");
-			
+
 		//Loading XML (Couldn't get it to load straight into a variable)
 		$("#xmlimport").load('http://www.esvapi.org/v2/rest/passageQuery?key=c132b8e2f4f975cc&passage=' + passage[0] + '+' + passage[1] + '&include-xml-declaration=true&output-format=crossway-xml-1.0&include-simple-entities=true', function(responseText, textStatus, req) {
 			if (textStatus == "error") {
@@ -13,34 +13,37 @@ var fetchverses = function(callBack) {
 				var xml = $("#xmlimport").html();
 				    xmlDoc = $.parseXML( xml );
 				    $xml = $( xmlDoc );
+				
+					//Sorting XML contents
 				  	$xml.find("heading").remove();
 					$chapter = $xml.find("reference").text().split(":")[0];
 				    $flashcards = $xml.find( "verse-unit" );
-				//What goes into each
+				
 				var scripturedata = new Array();
 
-
-				if ($flashcards.length < 1) { //Checking for flashcards if not, showing error message
+				if ($flashcards.length < 1) { //Checking for flashcards,if none showing error message
 					$("#title").text("Sorry, we couldn't find that passage");
 				} else {
 					$flashcards.each(function(index) {
-
+						
 						var verseReference = $chapter + ":" + $(this).find("verse-num").text();
 						//Removing Extra XML that is rendered after referencing the verse
 						$(this).find("verse-num, footnote").remove();
-						var audionum = $(this).attr("unit-id");
+						var audionum = $(this).attr("unit-id"); //audio number for mp3
 						var data = { reference : verseReference,
 						 			 versecontents : $(this).text(),
 						 			 audiolink : "http://stream.esvmedia.org/mp3-play/hw/" + audionum + ".mp3" };
 						scripturedata[index] = data;
 					});
 
-					localStorage.previousScriptures = JSON.stringify(scripturedata);
-					$("#title").text("Studying " + localStorage.previousPassage);
-
+					localStorage.Scriptures = JSON.stringify(scripturedata); //puting scriptures in local storage
+					
+					//Calling Callback
 					if (callBack != undefined) {
 						callBack();
 					}
+					
+					$("#title").text("Studying " + localStorage.Passage);
 				}//End Flashcard checking
 				
 			} //End Connection Checking
@@ -53,7 +56,7 @@ var initpassages = function(prevInit) {
 	
 	//Needed to create a seperate function to call twice in each case and after the retrieval of the verses
 	var commonInstructions = function() {
-			var scriptures = JSON.parse(localStorage.previousScriptures); //Stores the scriptures to be rendered
+			var scriptures = JSON.parse(localStorage.Scriptures); //Stores the scriptures to be rendered
 			
 			//Rendering Scripture Partial
 			renderPartial(scriptures, "partials/flashcard.html", "#scriptures", function(){
@@ -71,30 +74,35 @@ var initpassages = function(prevInit) {
 		
 	
 		if (prevInit === "true") { //If this is a previous passage reload
-			$("#passage").val(localStorage.previousPassage);
+			$("#passage").val(localStorage.Passage);
 			commonInstructions();
-			$("#title").text("Studying " + localStorage.previousPassage);
+			$("#title").text("Studying " + localStorage.Passage);
 		} else { //If it a new passage
-			localStorage.previousPassage = $("#passage").val();
+			localStorage.Passage = $("#passage").val();
 			fetchverses(commonInstructions);
 		};
 
 };
 
+//Made the renderPartial function reusable for any future improvements, etc.
 var renderPartial = function(data, partialLocation, destination, everyRenderCallback, finalCallback){
 	
 	$("#partialloading").load(partialLocation, function(){
 		var source = $("#partialloading").html();
 		var template = Handlebars.compile(source);
 		var compiledHTML = "";
-		$.when(
+		
+		//Had to what for the function calls using when / then because of asynchronous Javascript execution in Safari
+		$.when(		
+			//compiling each verse
 			$.each(data, function(i, linedata){
 				compiledHTML = compiledHTML + template(linedata);
 				if (everyRenderCallback != undefined) {
 					everyRenderCallback();
 				}
 			})	
-		).then(function(){
+		).then(function(){		
+			//Inserting HTML into #scripture div
 			$(destination).html(compiledHTML);
 		}).then(function(){
 			if (finalCallback != undefined) {
